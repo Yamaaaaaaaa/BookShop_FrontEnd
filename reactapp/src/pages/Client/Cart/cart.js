8ae3031849a96bc32ca2daa36c6a19b6c876548e
Kaita <110320455+Yamaaaaaaaa@users.noreply.github.com>
2025-03-09 04:29:56 +0700

    Code: Update Cart

--- reactapp/src/pages/Client/Cart/cart.js
+++ reactapp/src/pages/Client/Cart/cart.js
@@ -1,51 +1,66 @@
 import { useEffect, useState } from "react"
 import "./cart.scss"
-import { getCartforUser } from "../../../service/userService"
+import { getCartForUser, updateCartForUser } from "../../../service/userService"
+import { toast } from "react-toastify"
 
-const mock_items = [
-    {
-        id: 1,
-        name: "Battle Drive",
-        price: 27.00,
-        quantity: 5
-    },
-    {
-        id: 2,
-        name: "Battle Tank",
-        price: 22.00,
-        quantity: 2
-    },
-    {
-        id: 3,
-        name: "Shimmer",
-        price: 218.00,
-        quantity: 3
-    },
-    {
-        id: 4,
-        name: "Conclusion",
-        price: 228.00,
-        quantity: 4
-    }
-]
 
 const Cart = () => {
     const [userCart, setUserCart] = useState([])
     const userID = JSON.parse(sessionStorage.getItem("user")).id
     console.log("userID", userID);
-    
-    useEffect(() => {
-        const getCart = async () => {
-            const responseGetCart = await getCartforUser(userID)
-            console.log("responseGetCart", responseGetCart);
-            
-            if(responseGetCart && responseGetCart.status === 1 && responseGetCart.data){
-                setUserCart(responseGetCart.data)
-                console.log("userCart",responseGetCart.data);
-            }else{
-                setUserCart([])
+
+
+    const getCart = async () => {
+        const responseGetCart = await getCartForUser(userID)
+        console.log("responseGetCart", responseGetCart);
+        
+        if(responseGetCart && responseGetCart.status === 1 && responseGetCart.data){
+            setUserCart(responseGetCart.data)
+            console.log("userCart",responseGetCart.data);
+        }else{
+            setUserCart([])
+        }
+    }
+    const handleUpdateCart = async (cartID, action) => {
+        const response = await updateCartForUser(cartID, action)
+        console.log("Response Update Cart: ", response);
+        
+        if(response){
+            if(+response.status === 1) 
+            {
+                toast.success(response.message);
+                getCart()
+            }
+            else {
+                toast.error(response.message, {
+                    position: "top-right",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: false,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "light",
+                    transition: "Bounce",
+                });
             }
         }
+        else 
+        {
+            toast.error(response.message, {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: false,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "light",
+                transition: "Bounce",
+            });
+        }
+    }
+    useEffect(() => {
         getCart()
     }, [userID])
     return (
@@ -75,13 +90,13 @@ const Cart = () => {
                                     <td className="cart_price">${item.Book.sale.toFixed(2)}</td>
                                     <td>
                                         <div className="quantity-control">
-                                            <button className="quantity-btn">-</button>
+                                            <button className="quantity-btn" disabled={item.quantity === 0} onClick={() => handleUpdateCart(item.id, "down")}>-</button>
                                             <input type="text" value={item.quantity} className="quantity-input"/>
-                                            <button className="quantity-btn">+</button>
+                                            <button className="quantity-btn" disabled={item.quantity + 1 >= item.Book.stock} onClick={() => handleUpdateCart(item.id, "up")}>+</button>
                                         </div>
                                     </td>
                                     <td className="cart_price">${(item.Book.sale * item.quantity).toFixed(2)}</td>
-                                    <td><button className="close-btn">×</button></td>
+                                    <td><button className="close-btn" onClick={() => handleUpdateCart(item.id, "delete")}>×</button></td>
                                 </tr>
                             ))
                         }
--- reactapp/src/service/userService.js
+++ reactapp/src/service/userService.js
@@ -1,8 +1,14 @@
 import instance from "../setup/axiosCF"
 
 export const mainUrl = "http://localhost:8080/api/v1"
-export const getCartforUser = async (userId) => {
+export const getCartForUser = async (userId) => {
     return await instance.get(mainUrl + "/user/get-cart", {
         params: {userId}
     })
 }
+
+export const updateCartForUser = async (cartID, action) => {
+    return await instance.put(mainUrl + "/user/update-cart", {}, { // Body rỗng (Tk Put nó nhận đó)
+        params: { cartID, action }
+    });
+};
